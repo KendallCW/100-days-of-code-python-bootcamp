d3b12bb940f2976513643803e04a13ec124c8d9d
Kendall Castro <130414862+KendallCW@users.noreply.github.com>
2025-07-09 13:19:13 -0600

    Update band_name_generator.py

--- Day01/band_name_generator.py
+++ Day01/band_name_generator.py
@@ -1,1 +1,4 @@
-
+print("Greetings Stranger!")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name could be: " + city + " " + pet)
