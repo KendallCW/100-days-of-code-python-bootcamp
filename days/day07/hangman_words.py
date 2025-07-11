371d9fa02f5bfe2785d4a6b965e07587500c453a
Kendall Castro <130414862+KendallCW@users.noreply.github.com>
2025-07-11 11:09:05 -0600

    Create hangman_words.py

---
+++ days/day07/hangman_words.py
@@ -0,0 +1,13 @@
+# hangman_words.py
+# Lista de palabras para el juego
+
+word_list = [
+    "abruptly", "absurd", "askew", "awkward", "bagpipes", "banjo", "blizzard",
+    "bookworm", "boxcar", "buffalo", "buzzard", "cycle", "dwarves", "equip",
+    "fishhook", "galaxy", "glyph", "icebox", "injury", "jackpot", "jazzy",
+    "jigsaw", "jinx", "jukebox", "kayak", "klutz", "luxury", "matrix",
+    "mnemonic", "oxygen", "pixel", "pizazz", "psyche", "quartz", "rhythm",
+    "sphinx", "spritz", "strength", "subway", "topaz", "unzip", "voodoo",
+    "wave", "waxy", "wellspring", "whiskey", "wizard", "xylophone", "yippee",
+    "zombie"
+]
